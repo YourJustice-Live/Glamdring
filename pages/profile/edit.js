e4a520bc5cb9663e0d5a9d9ab66d8899d08782b1
Arthur Sabirzyanov <vampirenish666@gmail.com>
2022-03-17 12:42:26 +0300

    Fix names for edit profile page

--- pages/profile/edit.js
+++ pages/profile/edit.js
@@ -1,9 +1,9 @@
 import { Typography } from '@mui/material';
 import Layout from 'components/layout/Layout';
 
-export default function ProfileCreate() {
+export default function ProfileEdit() {
   return (
-    <Layout title={"YourJustice / Profile Create"}>
+    <Layout title={"YourJustice / Profile Edit"}>
 
       <Typography variant='h4' gutterBottom>Edit Own Profile</Typography>
       <Typography variant='h5'>...</Typography>
