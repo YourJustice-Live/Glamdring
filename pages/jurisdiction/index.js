5030bcdfedf2e69e68e9f0452e12dbbfc1b9ada2
Arthur Sabirzyanov <vampirenish666@gmail.com>
2022-03-27 17:00:22 +0300

    Improve UI for the jurisdiction page

--- pages/jurisdiction/index.js
+++ pages/jurisdiction/index.js
@@ -1,6 +1,7 @@
-import { Button, Divider, Skeleton, Typography, Stack } from '@mui/material';
+import { Save } from '@mui/icons-material';
+import { LoadingButton } from '@mui/lab';
+import { Button, Divider, Skeleton, Stack, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import LoadingBackdrop from 'components/extra/LoadingBackdrop';
 import RoleManager from 'components/jurisdiction/RoleManager';
 import Layout from 'components/layout/Layout';
 import ProfileList from 'components/profile/ProfileList';
@@ -14,35 +15,44 @@ import { useEffect, useState } from 'react';
 import { formatAccount } from "utils/formatters";
 
 /**
- * Page with jurisdiction data.
+ * Page with the jurisdiction.
  */
 export default function Jurisdiction() {
 
   const { enqueueSnackbar } = useSnackbar();
-  const { account } = useAccount();
   const { getName, getOwner, isHasRole, join, leave } = useJuridictionContract();
   const { findJurisdictionParticipantEntities } = useSubgraph();
   const { getProfiles } = useProfile();
-  const [isLoading, setIsLoading] = useState(true);
-  const [name, setName] = useState(null);
-  const [owner, setOwner] = useState(null);
-  const [isMember, setIsMember] = useState(null);
-  const [isJudge, setIsJudge] = useState(null);
-  const [isAdmin, setIsAdmin] = useState(null);
+  const { account } = useAccount();
+
+  const [basicInformation, setBasicInformation] = useState(null);
   const [memberProfiles, setMemberProfiles] = useState(null);
   const [judgeProfiles, setJudgeProfiles] = useState(null);
   const [adminProfiles, setAdminProfiles] = useState(null);
+  const [isJoiningOrLeaving, setIsJoiningOrLeaving] = useState(false);
 
-  async function loadData() {
+  async function loadBasicInformation() {
     try {
-      setIsLoading(true);
-      setName(await getName());
-      setOwner(await getOwner());
-      if (account) {
-        setIsMember(await isHasRole(account, JURISDICTION_ROLE.member));
-        setIsJudge(await isHasRole(account, JURISDICTION_ROLE.judge));
-        setIsAdmin(await isHasRole(account, JURISDICTION_ROLE.admin));
-      }
+      const name = await getName();
+      const owner = await getOwner();
+      const isAccountMember = account ? await isHasRole(account, JURISDICTION_ROLE.member) : null;
+      const isAccountJudge = account ? await isHasRole(account, JURISDICTION_ROLE.judge) : null;
+      const isAccountAdmin = account ? await isHasRole(account, JURISDICTION_ROLE.admin) : null;
+      setBasicInformation({
+        name: name,
+        owner: owner,
+        isAccountMember: isAccountMember,
+        isAccountJudge: isAccountJudge,
+        isAccountAdmin: isAccountAdmin
+      });
+    } catch (error) {
+      console.error(error);
+      enqueueSnackbar(`Oops, error: ${error}`, { variant: 'error' });
+    }
+  }
+
+  async function loadParticipants() {
+    try {
       const participants = await findJurisdictionParticipantEntities();
       const memberProfiles = await getProfiles(participants.filter(participant => participant?.isMember).map(participant => participant?.id));
       const judgeProfiles = await getProfiles(participants.filter(participant => participant?.isJudge).map(participant => participant?.id));
@@ -53,122 +63,114 @@ export default function Jurisdiction() {
     } catch (error) {
       console.error(error);
       enqueueSnackbar(`Oops, error: ${error}`, { variant: 'error' });
+    }
+  }
+
+  async function joinOrLeave() {
+    try {
+      setIsJoiningOrLeaving(true);
+      let transaction;
+      if (basicInformation.isAccountMember) {
+        transaction = await leave();
+      } else {
+        transaction = await join();
+      }
+      enqueueSnackbar("Success! Data will be updated soon.", { variant: 'success' });
+      await transaction.wait();
+      await loadBasicInformation();
+    }
+    catch (error) {
+      console.error(error);
+      enqueueSnackbar(`Oops, error: ${error}`, { variant: 'error' });
     } finally {
-      setIsLoading(false);
-    }
-  }
-
-  async function joinJurisdiction() {
-    try {
-      setIsLoading(true);
-      const transaction = await join();
-      await transaction.wait();
-      enqueueSnackbar("You joined the jurisdiction!", { variant: 'success' });
-      loadData();
-    }
-    catch (error) {
-      console.error(error);
-      enqueueSnackbar(`Oops, error: ${error}`, { variant: 'error' });
-      setIsLoading(false);
-    }
-  }
-
-  async function leaveJurisdiction() {
-    try {
-      setIsLoading(true);
-      const transaction = await leave();
-      await transaction.wait();
-      enqueueSnackbar("You left the jurisdiction!", { variant: 'success' });
-      loadData();
-    }
-    catch (error) {
-      console.error(error);
-      enqueueSnackbar(`Oops, error: ${error}`, { variant: 'error' });
-      setIsLoading(false);
+      setIsJoiningOrLeaving(false);
     }
   }
 
   useEffect(() => {
-    loadData();
+    loadBasicInformation();
+    loadParticipants();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
     <Layout title={"YourJustice / Jurisdiction"} showAccountNavigation={!!account}>
-      {isLoading ? (
-        <>
-          <LoadingBackdrop />
-          <Skeleton variant="rectangular" height={32} width={256} />
-          <Skeleton variant="rectangular" height={18} width={198} sx={{ mt: 1 }} />
-          <Skeleton variant="rectangular" height={18} width={64} sx={{ mt: 1 }} />
-        </>
-      ) : (
-        <>
-          <Box>
-            <Typography variant='h1' gutterBottom>Jurisdiction</Typography>
-            <Divider sx={{ mb: 3 }} />
-            <Typography gutterBottom><b>Name: </b>{name || "None"}</Typography>
-            <Typography gutterBottom><b>Owner: </b>{formatAccount(owner) || "None"}</Typography>
+      <Box>
+        <Typography variant='h1' gutterBottom>Jurisdiction</Typography>
+        <Divider sx={{ mb: 3 }} />
+        {basicInformation ? (
+          <>
+            <Typography gutterBottom><b>Name: </b>{basicInformation.name}</Typography>
+            <Typography gutterBottom><b>Owner: </b>{formatAccount(basicInformation.owner)}</Typography>
             {account && (
               <>
-                <Typography gutterBottom><b>Account is member: </b>{isMember ? "yes" : "no"}</Typography>
-                <Typography gutterBottom><b>Account is judge: </b>{isJudge ? "yes" : "no"}</Typography>
-                <Typography gutterBottom><b>Account is admin: </b>{isAdmin ? "yes" : "no"}</Typography>
+                <Typography gutterBottom><b>Account is member: </b>{basicInformation.isAccountMember ? "yes" : "no"}</Typography>
+                <Typography gutterBottom><b>Account is judge: </b>{basicInformation.isAccountJudge ? "yes" : "no"}</Typography>
+                <Typography gutterBottom><b>Account is admin: </b>{basicInformation.isAccountAdmin ? "yes" : "no"}</Typography>
               </>
             )}
-          </Box>
-          {account && (
-            <Box sx={{ mt: 6 }}>
-              <Typography variant='h4' gutterBottom>Actions</Typography>
-              <Divider sx={{ mb: 2.5 }} />
-              <Stack direction="row" spacing={1}>
-                {!isMember && (
-                  <Button variant="contained" type="submit" onClick={joinJurisdiction}>Join</Button>
-                )}
-                {isMember && (
-                  <Button variant="contained" type="submit" onClick={leaveJurisdiction}>Leave</Button>
-                )}
-                {isAdmin && (
-                  <RoleManager />
-                )}
-              </Stack>
-            </Box>
-          )}
-          <Box sx={{ mt: 6 }}>
-            <Typography variant='h4' gutterBottom>Members</Typography>
-            <Divider sx={{ mb: 2.5 }} />
-            <ProfileList
-              profiles={memberProfiles}
-              onUpdateProfiles={() => {
-                setMemberProfiles(null);
-                loadData();
-              }}
-            />
-          </Box>
-          <Box sx={{ mt: 6 }}>
-            <Typography variant='h4' gutterBottom>Judges</Typography>
-            <Divider sx={{ mb: 2.5 }} />
-            <ProfileList
-              profiles={judgeProfiles}
-              onUpdateProfiles={() => {
-                setJudgeProfiles(null);
-                loadData();
-              }}
-            />
-          </Box>
-          <Box sx={{ mt: 6 }}>
-            <Typography variant='h4' gutterBottom>Admins</Typography>
-            <Divider sx={{ mb: 2.5 }} />
-            <ProfileList
-              profiles={adminProfiles}
-              onUpdateProfiles={() => {
-                setAdminProfiles(null);
-                loadData();
-              }}
-            />
-          </Box>
-        </>
+          </>
+        ) : (
+          <>
+            <Skeleton variant="rectangular" height={24} width={256} sx={{ mb: 1 }} />
+            <Skeleton variant="rectangular" height={24} width={256} sx={{ mb: 1 }} />
+          </>
+        )}
+      </Box>
+      {basicInformation && account && (
+        <Box sx={{ mt: 6 }}>
+          <Typography variant='h4' gutterBottom>Actions</Typography>
+          <Divider sx={{ mb: 2.5 }} />
+          <Stack direction="row" spacing={1}>
+            {isJoiningOrLeaving && (
+              <LoadingButton loading loadingPosition="start" startIcon={<Save />} variant="outlined">
+                {basicInformation.isAccountMember ? "Leaving" : "Joining"}
+              </LoadingButton>
+            )}
+            {!isJoiningOrLeaving && (
+              <Button variant="contained" type="submit" onClick={joinOrLeave}>
+                {basicInformation.isAccountMember ? "Leave" : "Join"}
+              </Button>
+            )}
+            {basicInformation.isAccountAdmin && (
+              <RoleManager />
+            )}
+          </Stack>
+        </Box>
       )}
+      <Box sx={{ mt: 6 }}>
+        <Typography variant='h4' gutterBottom>Members</Typography>
+        <Divider sx={{ mb: 2.5 }} />
+        <ProfileList
+          profiles={memberProfiles}
+          onUpdateProfiles={() => {
+            setMemberProfiles(null);
+            loadData();
+          }}
+        />
+      </Box>
+      <Box sx={{ mt: 6 }}>
+        <Typography variant='h4' gutterBottom>Judges</Typography>
+        <Divider sx={{ mb: 2.5 }} />
+        <ProfileList
+          profiles={judgeProfiles}
+          onUpdateProfiles={() => {
+            setJudgeProfiles(null);
+            loadData();
+          }}
+        />
+      </Box>
+      <Box sx={{ mt: 6 }}>
+        <Typography variant='h4' gutterBottom>Admins</Typography>
+        <Divider sx={{ mb: 2.5 }} />
+        <ProfileList
+          profiles={adminProfiles}
+          onUpdateProfiles={() => {
+            setAdminProfiles(null);
+            loadData();
+          }}
+        />
+      </Box>
     </Layout >
   )
 
