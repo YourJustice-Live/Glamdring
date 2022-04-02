45ff3c9db20aa52e60fbc9eae67cebe1aa06b2c4
Arthur Sabirzyanov <vampirenish666@gmail.com>
2022-04-02 09:44:15 +0300

    Fix link tag for building

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
-        ></link>
+        />
       </Head>
       <body>
         <Main />
