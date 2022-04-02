19e3685234482cecd03a68242f64ae7e9a525fb6
Arthur Sabirzyanov <vampirenish666@gmail.com>
2022-04-02 17:08:22 +0300

    Fix link tag in document

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,11 @@ export default function Document() {
         />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
