daf4f186889bcd859e9058e2795e688bb4ba51f0
Arthur Sabirzyanov <vampirenish666@gmail.com>
2022-04-03 20:35:56 +0300

    Update next.config.js for fix rjsf warning

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
+// Fix for rjsf (https://github.com/rjsf-team/react-jsonschema-form/issues/2762)
+module.exports = {
+  webpack(config) {
+    config.resolve.fallback = {
+      '@mui/material': false,
+      '@mui/icons-material': false,
+    };
+    return config;
+  },
+};
+
 module.exports = nextConfig;
