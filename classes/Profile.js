1dd48ea2c2fcc79a1e0d79a4190e4714b06346a2
Arthur Sabirzyanov <vampirenish666@gmail.com>
2022-04-05 19:21:22 +0300

    Create classes for action, profile, rule instead of functions

---
+++ classes/Profile.js
@@ -0,0 +1,11 @@
+/**
+ * Class for the profile.
+ */
+export default class Profile {
+  constructor(account, avatarNftId, avatarNftReputations, avatarNftMetadata) {
+    this.account = account;
+    this.avatarNftId = avatarNftId;
+    this.avatarNftReputations = avatarNftReputations;
+    this.avatarNftMetadata = avatarNftMetadata;
+  }
+}
