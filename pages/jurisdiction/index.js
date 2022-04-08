e609d56230dd9b4508f4d3072d84138856021742
Arthur Sabirzyanov <vampirenish666@gmail.com>
2022-04-08 15:51:49 +0300

    [5250e] Restore jurisdiction page

---
+++ pages/jurisdiction/index.js
@@ -0,0 +1,29 @@
+import JurisdictionCases from 'components/jurisdiction/JurisdictionCases';
+import JurisdictionCaseCreator from 'components/jurisdiction/JurisdictionCaseCreator';
+import JurisdictionMembers from 'components/jurisdiction/JurisdictionMembers';
+import JurisdictionMeta from 'components/jurisdiction/JurisdictionMeta';
+import JurisdictionOfficials from 'components/jurisdiction/JurisdictionOfficials';
+import JurisdictionLaws from 'components/jurisdiction/JurisdictionLaws';
+import Layout from 'components/layout/Layout';
+import useWeb3Context from 'hooks/useWeb3Context';
+
+/**
+ * Page with the jurisdiction.
+ */
+export default function Jurisdiction() {
+  const { account } = useWeb3Context();
+
+  return (
+    <Layout
+      title={'YourJustice / Jurisdiction'}
+      showAccountNavigation={!!account}
+    >
+      <JurisdictionMeta />
+      <JurisdictionCaseCreator />
+      <JurisdictionCases />
+      <JurisdictionOfficials />
+      <JurisdictionMembers />
+      <JurisdictionLaws />
+    </Layout>
+  );
+}
