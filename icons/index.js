72912d93ae4e694c258c2cfe78ed2e375353de04
Arthur Sabirzyanov <vampirenish666@gmail.com>
2022-04-11 13:17:46 +0300

    Refactor icons, update pallete colors

--- icons/index.js
+++ icons/index.js
@@ -1,6 +1,5 @@
-export { default as Logo } from './logo.svg';
-export { default as IconRoadMap } from './roadmapIcon.svg';
-export { default as IconWallet } from './walletIcon.svg';
-export { default as IconHome } from './IconHome.svg';
-export { default as IconPlus } from './IconPlus.svg';
+export { IconHome } from './IconHome';
+export { IconPlus } from './IconPlus';
 export { IconProfile } from './IconProfile';
+export { IconWallet } from './IconWallet';
+export { Logo } from './Logo';
