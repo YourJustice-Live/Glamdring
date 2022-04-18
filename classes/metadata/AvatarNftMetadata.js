692b796db1818cfe16dd6738ce304c52b3925bbe
Arthur Sabirzyanov <vampirenish666@gmail.com>
2022-04-18 22:04:21 +0300

    Add class for avatar nft metadata

---
+++ classes/metadata/AvatarNftMetadata.js
@@ -0,0 +1,9 @@
+/**
+ * Class for metadata of avatar nft.
+ */
+export default class AvatarNftMetadata {
+  constructor(image, attributes) {
+    this.image = image;
+    this.attributes = attributes;
+  }
+}
