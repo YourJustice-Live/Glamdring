0b91c5f4bafd376520cbb0db1c2692443199db11
Arthur Sabirzyanov <vampirenish666@gmail.com>
2022-05-06 09:06:26 +0300

    [d02ea] Disable feature to invite another persons because of contracts bug

--- pages/profile/invite.js
+++ pages/profile/invite.js
@@ -1,13 +1,28 @@
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import Layout from 'components/layout/Layout';
 import ProfileManage from 'components/profile/ProfileManage';
 
 /**
  * Page where account can create profile for another person.
  */
-export default function ProfileCreate() {
+export default function ProfileInvite() {
+  const isFeatureEnabled = false;
+
   return (
     <Layout title={'YourJustice / Invite Person'} enableSidebar={true}>
-      <ProfileManage action="createAnotherProfile" />
+      {isFeatureEnabled ? (
+        <ProfileManage action="createAnotherProfile" />
+      ) : (
+        <Box>
+          <Typography variant="h1" gutterBottom>
+            Feature is under development
+          </Typography>
+          <Typography gutterBottom>
+            The ability to invite another persons will appear very soon..
+          </Typography>
+        </Box>
+      )}
     </Layout>
   );
 }
